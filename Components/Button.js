e119d6d3061f53d4515d2f6f6348482bd260da57
Guido Glielmi <guidoglielmi@gmail.com>
2022-01-30 23:43:55 -0300

    Several designs improvements on product component

--- Components/Button.js
+++ Components/Button.js
@@ -1,17 +1,17 @@
 import { useState } from 'react';
 import styles from './button.module.css';
-const Button = ({ children, backgroundColor, clickeable }) => {
+const Button = ({ children, backgroundColor, clickeable, size }) => {
 	const [toggleStyle, setToggleStyle] = useState(false);
 	return clickeable ? (
 		<div
 			onClick={() => setToggleStyle(!toggleStyle)}
 			style={{ background: toggleStyle ? backgroundColor : '#ddd', cursor: 'pointer' }}
-			className={`${toggleStyle ? styles.boxOn : styles.boxOff} rowContainer`}
+			className={`${toggleStyle ? styles.boxOn : styles.boxOff} ${size ? size : 'small'}Font rowContainer`}
 		>
 			{children}
 		</div>
 	) : (
-		<div style={{ cursor: 'default' }} className={`${styles.boxOff} rowContainer`}>
+		<div style={{ cursor: 'default' }} className={`${styles.boxOff} ${size ? size : 'small'}Font rowContainer`}>
 			{children}
 		</div>
 	);
--- Components/Product.js
+++ Components/Product.js
@@ -1,11 +1,31 @@
 import { useState } from 'react';
 import BuyBlue from '../public/assets/icons/buy-blue.svg';
 import BuyWhite from '../public/assets/icons/buy-white.svg';
+import Coin from '../public/assets/icons/coin.svg';
+import Button from './Button';
 import styles from './product.module.css';
 const Product = ({ src, title, category }) => {
 	const [clicked, setClicked] = useState(false);
-	return (
-		<div className={`${styles.box} columnContainer`} onClick={() => setClicked(!clicked)}>
+	return clicked ? (
+		<div className={`${styles.clickedBox} columnContainer`} onMouseOut={() => setClicked(!clicked)}>
+			<div className={`${styles.redeem} columnContainer`}>
+				<div className='rowContainer'>
+					<Coin />
+					<span className='mediumFont'>12000</span>
+				</div>
+				<Button size='small'>Redeem Now</Button>
+			</div>
+			{clicked ? <BuyWhite className={styles.buyIcon} /> : <BuyBlue className={styles.buyIcon} />}
+			<div className='reducedOpacity'>
+				<img src='/assets/product-pics/AcerAspire-x1.png' alt='' />
+			</div>
+			<div className={`${styles.info} reducedOpacity`}>
+				<h3 className={`${styles.category} xSmallFont`}>Phones</h3>
+				<h2 className={`${styles.title} smallFont`}>iPhone 8</h2>
+			</div>
+		</div>
+	) : (
+		<div className={`${styles.box} columnContainer`} onMouseOver={() => setClicked(!clicked)}>
 			{clicked ? <BuyWhite className={styles.buyIcon} /> : <BuyBlue className={styles.buyIcon} />}
 			<div>
 				<img src='/assets/product-pics/AcerAspire-x1.png' alt='' />
